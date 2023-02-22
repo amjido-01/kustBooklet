3da9a81fd5135c0a948abdfef7da4e007c9aaa5d
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-22 13:27:50 +0100

    commit

--- kust-book-let/src/Header2.jsx
+++ kust-book-let/src/Header2.jsx
@@ -2,6 +2,8 @@ import React from 'react'
 import { useState } from 'react'
 import { MenuItems } from './components/MenuItems'
 import {HiMenuAlt3} from 'react-icons/hi'
+import {RiMenu3Line} from 'react-icons/ri'
+import { Button } from './components/Button'
 
 export const Header2 = () => {
   const [active, setActive] = useState(false)
@@ -11,26 +13,31 @@ export const Header2 = () => {
   }
 
   return (
-    <div className='fixed w-full bg-green-600 text-white items-center flex justify-between text-center'>
+    <div className='bg-[#323232] fixed px-2 sm:px-4 py-2.5 z-20 top-0 left-0 border-2 w-full text-white items-center flex justify-around text-center'>
       <div className="logo text-center font-bold">
-        <span className=''> KustBooklet </span>
+        <a href="">
+         <span className=''> KUST<span className='text-[#0f9d58]'>Booklet</span></span>
+        </a>
       </div>
     
-      <nav>
-          <div className=' absolute right-6 md:hidden cursor-pointer scale-150 '>
-            <HiMenuAlt3 onClick={ handleNavbar } style={{ color: 'white'}} />
+     <div className='flex'>
+     <nav>
+          <div className=' absolute right-6 items-center text-center mb-4 flex sm:hidden cursor-pointer scale-150 '>
+            <RiMenu3Line onClick={ handleNavbar } style={{ color: 'white', textAlign: 'center'}} className='ico' />
           </div>
 
-        <ul className='hidden md:flex gap-8 p-6 uppercase'>
-          <li><a href=''>Home</a></li>
-          <li><a href=''>Handouts</a></li>
-          <li><a href=''>Past Questions</a></li>
-          <li><a href=''>Contact</a></li>
-        </ul>
+          <ul className='hidden sm:flex gap-8 p-4 uppercase md:text-sm md:font-bold font-popins rounded-lg text-[#f5f5f5]'>
+            <li className='hover:underline underline-offset-4'><a href=''>Home</a></li>
+            <li className='hover:underline underline-offset-4'><a href=''>Handouts</a></li>
+            <li className='hover:underline underline-offset-4'><a href=''>Past Questions</a></li>
+            <li className='hover:underline underline-offset-4'><a href=''>Contact</a></li>
+            <Button primaryGreen='#0f9d58'/>
+          </ul>
+      
 
-        <MenuItems handleNavbar={handleNavbar} active={active}></MenuItems>
-
+          <MenuItems handleNavbar={handleNavbar} active={active}></MenuItems>
       </nav>
+     </div>
     </div>
   )
 }
--- kust-book-let/src/components/Button.jsx
+++ kust-book-let/src/components/Button.jsx
@@ -2,7 +2,7 @@ import React from 'react'
 
 export const Button = ({primaryGreen}) => {
   return (
-    <button style={{backgroundColor: primaryGreen}}  className={`text-gray-800 font-bold font-popins ml-5  px-4 rounded inline-flex items-center`}>
+    <button style={{backgroundColor: primaryGreen}}  className={`text-gray-800 font-bold font-popins ml-5 py-1 px-4 rounded inline-flex items-center`}>
         <span className={`text-[#f5f5f5]`}>More Info</span>
     </button>
   )
