88b0a6be2edbc8c3f41b08b6c24b103551ac374f
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-22 15:20:43 +0100

    commit

--- kust-book-let/src/Header2.jsx
+++ kust-book-let/src/Header2.jsx
@@ -13,31 +13,34 @@ export const Header2 = () => {
   }
 
   return (
-    <div className='bg-[#323232] fixed px-2 sm:px-4 py-2.5 z-20 top-0 left-0 border-2 w-full text-white items-center flex justify-around text-center'>
-      <div className="logo text-center font-bold">
-        <a href="">
-         <span className=''> KUST<span className='text-[#0f9d58]'>Booklet</span></span>
-        </a>
-      </div>
-    
-     <div className='flex'>
-     <nav>
-          <div className=' absolute right-6 items-center text-center mb-4 flex sm:hidden cursor-pointer scale-150 '>
-            <RiMenu3Line onClick={ handleNavbar } style={{ color: 'white', textAlign: 'center'}} className='ico' />
-          </div>
+    <div className='bg-[#323232] fixed px-2 sm:px-4 py-2.5 z-20 top-0 left-0 border-2 w-full text-white flex '>
+     <div className='container flex flex-wrap items-center justify-around mx-auto'>
 
-          <ul className='hidden sm:flex gap-8 p-4 uppercase md:text-sm md:font-bold font-popins rounded-lg text-[#f5f5f5]'>
-            <li className='hover:underline underline-offset-4'><a href=''>Home</a></li>
-            <li className='hover:underline underline-offset-4'><a href=''>Handouts</a></li>
-            <li className='hover:underline underline-offset-4'><a href=''>Past Questions</a></li>
-            <li className='hover:underline underline-offset-4'><a href=''>Contact</a></li>
-            <Button primaryGreen='#0f9d58'/>
-          </ul>
+      <a href="" className='flex items-center border-3 border-green-600'>
+      <span className='self-center text-xl font-semibold whitespace-nowrap dark:text-white'> KUST<span className='text-[#0f9d58]'>Booklet</span></span>
+      </a>
+
       
+        <div className=' items-center justify-between border-3 border-green-600'>
 
-          <MenuItems handleNavbar={handleNavbar} active={active}></MenuItems>
-      </nav>
-     </div>
+
+            <div className=' absolute right-8 top-[18px] items-center text-center mb-4 flex sm:hidden cursor-pointer scale-150 '>
+              <RiMenu3Line onClick={ handleNavbar } style={{ color: 'white', textAlign: 'center'}} className='ico' />
+            </div>
+
+            <ul className='hidden sm:flex gap-8 p-4 w-full uppercase md:text-sm md:font-bold font-popins rounded-lg text-[#f5f5f5]'>
+              <li className='hover:underline underline-offset-4'><a href=''>Home</a></li>
+              <li className='hover:underline underline-offset-4'><a href=''>Handouts</a></li>
+              <li className='hover:underline underline-offset-4'><a href=''>Past Questions</a></li>
+              <li className='hover:underline underline-offset-4'><a href=''>Contact</a></li>
+              <Button primaryGreen='#0f9d58' ml='5px'/>
+            </ul>
+            
+              <MenuItems handleNavbar={handleNavbar} active={active}></MenuItems>
+          
+        </div>
+      </div>
+
     </div>
   )
 }
--- kust-book-let/src/components/MenuItems.jsx
+++ kust-book-let/src/components/MenuItems.jsx
@@ -1,14 +1,21 @@
 import React from 'react'
 import {RxCross1} from 'react-icons/rx'
+import { Button } from './Button'
 
 export const MenuItems = ({handleNavbar, active}) => {
   return (
-    <ul className={active ? 'flex flex-col  items-start p-3 fixed inset-0 left-[55%] uppercase h-[15rem] bg-[#F5F5F5] gap-1 ease-in-out duration-500 border-2 sm:hidden' : 'hidden'}>
-      <RxCross1 onClick={handleNavbar} className=' mt-7 ml-40 right-0 text-[#000000] font-medium text-[14px] leading-5 not-italic'/>
+   <div className={active ? 'flex flex-col items-start p-3 fixed inset-0 left-[55%] uppercase h-[15rem] bg-[#F5F5F5] gap-1 ease-in-out duration-500 border-2 sm:hidden' : 'hidden'}>
+     <ul className=''>
+      <RxCross1 onClick={handleNavbar} className='mt-7 ml-40 right-0 text-[#000000] font-medium text-[14px] leading-5 not-italic'/>
         <li className='text-[#000000] font-medium text-[14px] leading-5 not-italic'><a href=''>Home</a></li>
         <li className='text-[#000000] font-medium text-[14px] leading-5 not-italic'><a href=''>Handouts</a></li>
         <li className='text-[#000000] font-medium text-[14px] leading-5 not-italic'><a href=''>Past Questions</a></li>
         <li className='text-[#000000] font-medium text-[14px] leading-5 not-italic'><a href=''>Contact</a></li>
     </ul>
+    <div  className='flex justify-start'>
+    <Button primaryGreen='#0f9d58'/>
+
+    </div>
+   </div>
   )
 }
