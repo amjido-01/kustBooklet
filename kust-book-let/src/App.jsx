846e4a3c1f0ad548f67154e85e7fb04758c466e3
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-23 18:19:37 +0100

    commit

--- kust-book-let/src/App.jsx
+++ kust-book-let/src/App.jsx
@@ -1,16 +1,27 @@
-import { useState } from 'react'
+import { useState, useEffect } from 'react'
 import './App.css'
 import { Header2 } from './Header'
 import { Hero } from './components/Hero'
+import { Loding } from './components/Loding.jsx'
+
 
 function App() {
-  const [count, setCount] = useState(0)
-  
+  const [isLoading, setIsLoading] = useState(true)
+
+  useEffect(() => {
+    setTimeout(() => {
+      setIsLoading(false)
+    }, 8000)
+  })
 
   return (
-    <div>
-      <Header2 />
-      < Hero />
+   <div>
+      {
+        isLoading ? (<Loding />) : (<div>
+            <Header2 />
+            < Hero />
+        </div>)
+      }
     </div>
   )
 }
---
+++ kust-book-let/src/components/Loding.jsx
@@ -0,0 +1,22 @@
+import React from 'react'
+import { useState, useEffect } from 'react'
+import { Puff } from 'react-loader-spinner'
+
+
+export const Loding = () => {
+
+  return (
+   <div>
+        <Puff
+            height="80"
+            width="80"
+            radius={1}
+            color="#4fa94d"
+            ariaLabel="puff-loading"
+            wrapperStyle={{}}
+            wrapperClass=""
+            visible={true}
+        />
+   </div>
+  )
+}
