66624b2f25d497a2f361ff905e6419570af6a189
alajido <96017433+Alajido@users.noreply.github.com>
2023-02-25 12:33:12 +0100

    commit

--- kust-book-let/src/components/Content.jsx
+++ kust-book-let/src/components/Content.jsx
@@ -1,16 +1,16 @@
 import React from 'react'
 
-const Content = () => {
+const Content = (props) => {
   return (
-    <div className='text-white'>
+    <div className='text-[#1F1F1F] w-[95%] rounded-md  my-4 mx-auto' style={{backgroundColor: props.bg}}>
         <h1 className=' font-bold font-popins'>Handouts</h1>
-        <ul>
-            <li><a href=''>faculty of computing and mathematical science</a></li>
-            <li><a href=''></a></li>
-            <li><a href=''></a></li>
-            <li><a href=''></a></li>
-            <li><a href=''></a></li>
-            <li><a href=''></a></li>
+        <ul className='list-disc' style={{listStyleType: 'disc'}}>
+            <li className=' capitalize'><a href=''>faculty of computing and mathematical science (FACMS)</a></li>
+            <li className='capitalize'><a href=''>faculty of engineering (FAENG)</a></li>
+            <li className='capitalize'><a href=''>faculty of agriculture and agricultural technology (FAAT)</a></li>
+            <li className='capitalize'><a href=''>faculty of sciences (FASCI)</a></li>
+            <li className='capitalize'><a href=''>faculty of science of technology education (FAsCITE)</a></li>
+            <li className='capitalize'><a href=''>faculty of earth and environmental sciences</a></li>
         </ul>
     </div>
   )
--- kust-book-let/src/components/MainContent.jsx
+++ kust-book-let/src/components/MainContent.jsx
@@ -3,8 +3,9 @@ import Content from './Content'
 
 const MainContent = () => {
   return (
-    <div className='bg-[#1F1F1F]'>
-        <Content />
+    <div className='bg-[#1F1F1F] block md:flex'>
+        <Content bg='#98EBDB' />
+        <Content bg='#CB8ECC' />
     </div>
   )
 }
